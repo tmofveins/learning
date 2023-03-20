e754cb36c191ce0fefa9538061c9dd0302787445
tmofveins <nicksiow.zj@gmail.com>
2023-03-20 14:19:40 +0800

    lesson 1: added API skeleton code, 404 handling, home page router

---
+++ mern_project/server.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+const PORT = 3000
+
+// for the default page of our app, use resources from the /public folder on our system
+// powered by express - __dirname gives the absolute path
+app.use("/", express.static(path.join(__dirname, "/public")))
+
+app.use("/", require("./routes/root"))
+
+app.all("*", (req, res) => {
+    res.status(404);
+
+    if (req.accepts("html")) {
+        res.sendFile(path.join(__dirname, "views", "404.html"));
+    } else if (req.accepts("json")) {
+        res.json({
+            message: "404 Not Found",
+        })
+    } else {
+        res.type("txt").send("404 Not Found");
+    }
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
