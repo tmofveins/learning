cbdf0f282246159f3ac5b0d459f8520f9b674e88
tmofveins <nicksiow.zj@gmail.com>
2023-03-25 00:37:23 +0800

    lesson 5: added frontend

--- mern_project/backend/server.js
+++ mern_project/backend/server.js
@@ -14,7 +14,7 @@ const corsOptions = require("./config/corsOptions");
 const connectDB = require("./config/dbConn");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3500;
 
 connectDB();
 
